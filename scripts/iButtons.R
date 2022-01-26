b731d38589658c7c76f16de1912f5660b37834dc
Fiona  Beaty <fionabeaty@Fionas-MacBook-Pro.local>
2022-01-26 15:59:52 -0800

    began to incorporate alyssa's code into my ibutton stuff :)

--- scripts/iButtons.R
+++ scripts/iButtons.R
@@ -115,11 +115,98 @@ calvert_df <- rbind(DF_K1, DF_P1) %>%
   mutate(Time = format(Time_2, format = "%H:%M:%S")) %>% 
   select(Date, Time, SP, Temp)
 
-all_df <- rbind(nanaimo_df, calvert_df)
+#Merge into one cdataframe and add tidal height column based on RT_2_V1_Aug_KM excel sheet
+all_df <- rbind(nanaimo_df, calvert_df) %>% 
+  mutate(TideHeight = ifelse(SP == "Cedar", 1.15, 
+                             ifelse(SP == "Heron", 0.925,
+                                    ifelse(SP == "Kwak", 1.30,
+                                           ifelse(SP == "Pruth", 1.65, ""))))) %>% 
+  mutate(date.time = ymd_hms(paste(Date, Time)))
 
-#Export this to csv
-#write.csv(all_df, "cleaned_iButtons_2019.csv")
+
+#Export this to csv so that you can add in a column with the submerged/air manually 
+write.csv(all_df, "data/iButtons/cleaned_iButtons_2019.csv")
 
 #Remove unnecessary objects
 rm(calvert_df, Cedar_1, DF_C1, DF_H1, DF_K1, DF_P1, Heron_1, Kwak_1, nanaimo_df, Pruth_1,
    dates_excl, files, files_C1, files_H1, files_P1, i)
+
+#Manually assign 0 or 1 based on whether the marker is emmersed or not based on tide data from----
+#insert website here
+#The code in this section is informed by Alyssa's code
+#For the below code to run you need the column labels to read:
+#  date.time = col_character(),
+#  Unit = col_character(),
+#  Value = col_double(),
+#  `in.water` = col_integer()
+
+#*Output of below code*
+#  Three files with these variables:
+#  avgair= the average air temp (not suggested to be used for analysis - just as a reference)
+#  maxair=the absolute max air temp (could be used for analysis but generally this could be influenced by outliers)
+#  sdair=the standard deviation for variation in air temperature experienced
+#  air90th= the 90th quantile of air temperature experienced
+#  air99th=the 99th quantile of air temperature experienced (a better max temperature estimate)
+#  air10th=the 10th quantile of air temperature experienced
+
+#  avgwater= the average water temp (not suggested to be used for analysis - just as a reference)
+#  maxwater=the absolute max water temp (could be used for analysis but generally this could be influenced by outliers)
+#  sdwater=the standard deviation for variation in water temperature experienced
+#  water90th= the 90th quantile of water temperature experienced
+#  water99th=the 99th quantile of water temperature experienced (a better max temperature estimate)
+#  water10th=the 10th quantile of water temperature experienced
+#load in new dataframe with tide levels assigned
+
+#The code is slightly unnecessary, given that I have already merged my csvs into one file,
+#but I'm going to see whether it will still work on my one csv! As it's easier than re-writing it all
+#for just one file
+
+#remove all lists
+rm(list=ls(all=TRUE)) 
+
+#reads all the files in a single folder that have a .csv extension (change so that it goes to your files)
+fileNames <- Sys.glob("data/iButtons/cleaned_iButtons_2019.csv")
+
+#creates a list of the file names so that you can label them appropriately below (change so that it goes to your files)
+filename<-list.files(path="data/iButtons", pattern="csv")
+
+##create empty vectors
+f.air=NULL
+f.water=NULL
+airtemp=NULL
+watertemp=NULL
+
+
+# we'll be leaving the original files unaltered but using their data to create new files:
+for(i in 1:37){
+  sample <- read.csv(fileNames[i],
+                     header = TRUE)
+  
+  #seperate file by water and air
+  f.air<-sample %>% filter(in.water=="1")
+  f.water<-sample %>% filter(in.water=="0")
+  
+  ## creating columns to label sites and replicates based off our file name extension: 'BSP.1 iButton Data.csv', or "site.plot iButton Data.csv"
+  
+  #a column the length of water or air data that names which file the data came from
+  fileair<-rep(filename[i],length(f.air[,1]))
+  filewater<-rep(filename[i],length(f.water[,1]))
+  
+  #pull out station (col1) and plot number (col2)
+  sair1<-unlist(lapply(strsplit(as.character(fileair), " "),function(z) z[1]))
+  swater1<-unlist(lapply(strsplit(as.character(filewater), " "),function(z) z[1]))
+  
+  sair2<-strsplit(as.character(sair1), "[.]")
+  siteair<-do.call(rbind, sair2)
+  
+  swater2<-strsplit(as.character(swater1), "[.]")
+  sitewater<-do.call(rbind, swater2)
+  
+  #combine site label with ibutton data
+  f2.air<-cbind(siteair, f.air)  
+  f2.water<-cbind(sitewater, f.water)
+  
+  #stack the sites in rows
+  airtemp<-rbind(airtemp, f2.air)
+  watertemp<-rbind(watertemp, f2.water)
+}
