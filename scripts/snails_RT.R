ae7e31b6c36d04c3dee826e045f62da8c36de13a
Fiona  Beaty <fionabeaty@Fionas-MacBook-Pro.local>
2022-02-08 17:09:36 -0800

    Created figures :)

--- scripts/snails_RT.R
+++ scripts/snails_RT.R
@@ -1,7 +1,7 @@
 #Script to analyze & visualize the snail growth RVs (length, shell thickness etc) from the 2019 Reciprocal Transplant
 
 #Load packages----
-pkgs <- c("tidyverse", "viridis", "lubridate", "car")
+pkgs <- c("tidyverse", "viridis", "lubridate", "car", "visreg", "cowplot")
 lapply(pkgs, library, character.only = TRUE)
 rm(pkgs)
 
@@ -25,13 +25,13 @@ RV_clean <- RV_base %>%
 
 #Calculate & visualize the average & SD of growth metrics in the 3 time periods ----
 RV_sum_block <- RV_clean %>% 
-  group_by(Stage, OR, SP, Block) %>% 
-  summarize(meanL = mean(L, na.rm = TRUE), sdL = sd(L, na.rm = TRUE)) %>% 
+  group_by(Stage, OR, OS, SR, SP, Block) %>% 
+  summarize(meanL = mean(L, na.rm = TRUE), sdL = sd(L, na.rm = TRUE), nL = n()) %>% 
   ungroup()
 
 RV_sum_OR <- RV_sum_block %>% 
-  group_by(Stage, OR, SP) %>% 
-  summarize(meanL_OR = mean(meanL, na.rm = TRUE), sdL_OR = sd(meanL, na.rm = TRUE)) %>% 
+  group_by(Stage, OR, SR, SP) %>% 
+  summarize(meanL_OR = mean(meanL, na.rm = TRUE), sdL_OR = sd(meanL, na.rm = TRUE), nL_OR = n()) %>% 
   ungroup()
 
 RV_sum_cal <- RV_sum_OR %>% 
@@ -40,35 +40,75 @@ RV_sum_cal <- RV_sum_OR %>%
 RV_sum_nan <- RV_sum_OR %>% 
   filter(OR == "Nanaimo")
 
-my_theme <- theme(axis.title.x = element_text(size = 20), axis.text.x = element_text(size = 18),
-                  axis.text.y = element_text(size = 18), axis.title.y = element_text(size = 20), 
-                  legend.text = element_text(size = 20),
-                  panel.border = element_blank(), panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
-
-length_cal_stage<- ggplot(RV_sum_cal, aes(Stage, meanL_OR, group = SP, colour = SP)) + 
-  geom_point(aes(size = 0.5), position=position_dodge(0.3)) +
-  geom_line(aes(size= 0.3), position = position_dodge(0.3)) +
-  geom_errorbar(aes(ymin=meanL_OR-sdL_OR, ymax=meanL_OR+sdL_OR, size = 0.2), width=.2,
+length_cal_stage <- ggplot(RV_sum_cal, aes(Stage, meanL_OR, group = SP, colour = SP)) + 
+  geom_point(size = 3, position=position_dodge(0.3)) +
+  geom_line(size = 0.8, position = position_dodge(0.3), alpha = 0.5) +
+  geom_errorbar(aes(ymin=meanL_OR-sdL_OR, ymax=meanL_OR+sdL_OR), width=.2, size = 0.5,
                 position=position_dodge(0.3)) +
+  ylim(23.5, 39) +
   scale_colour_manual(values = c("coral", "coral3", "skyblue", "skyblue3")) +
-  theme_bw() + labs(x = "Stage, Calvert", y = "Mean Length") +
-  my_theme
+  labs(x = "Stage, Calvert", y = "Mean shell length (mm)") +
+  theme_cowplot(16)
 
 length_nan_stage <- ggplot(RV_sum_nan, aes(Stage, meanL_OR, group = SP, colour = SP)) + 
-  geom_point(aes(size = 0.5), position=position_dodge(0.3)) +
-  geom_line(aes(size = 0.3), position = position_dodge(0.3)) +
-  geom_errorbar(aes(ymin=meanL_OR-sdL_OR, ymax=meanL_OR+sdL_OR, size = 0.2), width=.2,
+  geom_point(size = 3, position=position_dodge(0.3)) +
+  geom_line(size = 0.8, position = position_dodge(0.3), alpha = 0.5) +
+  geom_errorbar(aes(ymin=meanL_OR-sdL_OR, ymax=meanL_OR+sdL_OR), width=.2, size = 0.5,
                 position=position_dodge(0.3)) +
   scale_colour_manual(values = c("coral", "coral3", "skyblue", "skyblue3")) +
-  theme_bw() + labs(x = "Stage, Nanaimo", y = "Mean Length") +
-  my_theme
+  ylim(23.5, 39) +
+  labs(x = "Stage, Nanaimo", y = "Mean shell length (mm)") +
+  theme_cowplot(16)
+
+length_combined_stage <- plot_grid(length_cal_stage + theme(legend.position = "none"), length_nan_stage + theme(axis.text.y = element_blank(),
+                                                   axis.ticks.y = element_blank(),
+                                                   axis.title.y = element_blank()),
+          axis = "b")
 
 ggsave(length_cal_stage, file = "plots/snails/RT/length_cal_stage.pdf", height = 4, width = 8, dpi = 300)
 ggsave(length_nan_stage, file = "plots/snails/RT/length_nan_stage.pdf", height = 4, width = 8, dpi = 300)
+ggsave(length_combined_stage, file = "plots/snails/RT/length_combined_stage.pdf", height = 4, width = 8, dpi = 300)
 
-#Test whether there's a significant effect of time (or an interesting one)
-anova_model <- aov(meanL ~ SP + Stage, data = sum_both)
-Anova(ancova_model, type="III")
-visreg(ancova_model)
+
+#Test whether there's a significant effect of time (or an interesting one)----
+anova_nan <- aov(meanL_OR ~ SP + Stage, data = RV_sum_nan)
+Anova(anova_nan, type="II")
+visreg(anova_nan)
+
+
+
+#Calculate & visualize the average & SD of growth metrics clumped by SR & OR----
+#First have to calculate the difference in growth metrics between final & initial
+RV_diff <- RV_sum_block %>% 
+  subset(Stage != "Mid") %>% 
+  arrange(OS, SP, Block, Stage) %>% 
+  group_by(Block) %>% 
+  mutate(diff_meanl = meanL - lag(meanL, default= meanL[1])) %>% 
+  subset(Stage == "Final") %>% 
+  ungroup()
+
+#Then summarize the mean difference grouped by OR & SR: there should be n = 16 for each grouping, and only 4 groupings
+RV_sum_OR_SR <- RV_diff %>% 
+  group_by(OR, SR) %>% 
+  summarize(meanL_OR_SR = mean(diff_meanl, na.rm = TRUE), sdL_OR_SR = sd(diff_meanl, na.rm = TRUE), 
+            nL_OR_SR = n()) %>% 
+  ungroup()
+
+length_OR_OS_box <- ggplot(RV_diff, aes(OR, diff_meanl, fill = SR, color = SR)) + 
+  geom_boxplot(color = "grey24", varwidth = TRUE) +
+  scale_fill_manual(values = c("coral", "skyblue")) +
+  labs(x = "Outplant Region", y = "Mean shell length (mm)") +
+  theme_cowplot(16)
+
+length_OR_OS_points <- ggplot(RV_sum_OR_SR, aes(OR, meanL_OR_SR, group = SR, colour = SR)) + 
+  geom_point(size = 3, position=position_dodge(0.3)) +
+  geom_line(size = 0.8, position = position_dodge(0.3), alpha = 0.5) +
+  geom_errorbar(aes(ymin=meanL_OR_SR-sdL_OR_SR, ymax=meanL_OR_SR+sdL_OR_SR), width=.2, size = 0.5,
+                position=position_dodge(0.3)) +
+  scale_colour_manual(values = c("coral", "skyblue")) +
+  labs(x = "Outplant region", y = "Mean shell length (mm)") +
+  theme_cowplot(16)
+
+ggsave(length_OR_OS_box, file = "plots/snails/RT/length_OR_OS_box.pdf", height = 4, width = 8, dpi = 300)
+ggsave(length_OR_OS_points, file = "plots/snails/RT/length_OR_OS_points.pdf", height = 4, width = 8, dpi = 300)
 
