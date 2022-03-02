6d6d6925e068ded5ef0c25883b80e36ba5b16a34
Fiona  Beaty <fionabeaty@Fionas-MacBook-Pro.local>
2022-03-01 17:58:17 -0800

    Created script for analyzing snail growth data

---
+++ scripts/snails_meso.R
@@ -0,0 +1,8 @@
+#Script to analyze snail response variables from the mesocosm experiment
+#Datasheets include the following measurements:
+#July 1st (initial): length, thickness, total weight
+#August 2nd (mid): submerged weight, total weight
+#Sept 2nd (final): length, thickness, total weight, submerged weight
+
+#Note: Because submerged weight was only collected in the middle of the experiment, changes to tissue & shell weight are only reflective of
+#the growth between mid & final stages (reference this in figure captions?)
